95aecaa0a6b34a8eaf4a0290cb0aa760ffdd420a
Ali Kılıçoğlu <73752182+AliKilicoglu@users.noreply.github.com>
2020-11-11 22:18:23 +0300

    1.8.2
    Hot fix

--- dosyasistemi.py
+++ dosyasistemi.py
@@ -8,32 +8,40 @@ import subprocess
 class DosyaSistemi:
 
 
-	def ac(self):
-		filename = QFileDialog.getOpenFileName()
-		self.path = filename[0]
-		return self.path
-	def yaz(self):
-		try:
-			dosya_konumu=DosyaSistemi.ac(self)
-			print(dosya_konumu)
-			dosya=open(dosya_konumu,"r")
-			return dosya
-		except:
-			pass
+    def ac(self):
+        filename = QFileDialog.getOpenFileName()
+    
+        self.path = filename[0]
 
-		
+        return self.path
+    def yaz(self):
+        try:
+            self.dosya_konumu=DosyaSistemi.ac(self)
 
-	def kaydet(self):
-		dosya_konumu = DosyaSistemi.ac(self)
-		dosya=open(dosya_konumu,"w")
-		return dosya
-	def calistir(self):
-		dosya_konumu=DosyaSistemi.ac(self)
-		output = subprocess.check_output([sys.executable, dosya_konumu]).decode('utf-8').rstrip()
+            dosya=open(self.dosya_konumu,"r")
 
-		msg = QMessageBox()
-		msg.setWindowTitle("Kod Derleniyor")
-		msg.setText("Kod Derleniyor")
-		msg.setIcon(QMessageBox.Question)
-		msg.setDetailedText(output)
-		x = msg.exec_()
+            return dosya
+        except:
+            pass
+    def dosya_adi(self):
+        return self.path
+        pass
+
+
+
+
+    def kaydet(self):
+
+        dosya=open(self.dosya_konumu,"w")
+        return dosya
+    def calistir(self):
+
+
+        output = subprocess.check_output([sys.executable, self.dosya_konumu]).decode('utf-8').rstrip()
+
+        msg = QMessageBox()
+        msg.setWindowTitle("Kod Derleniyor")
+        msg.setText("Kod Derleniyor")
+        msg.setIcon(QMessageBox.Question)
+        msg.setDetailedText(output)
+        x = msg.exec_()
