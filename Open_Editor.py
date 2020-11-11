389027e94a7320dd544f2c9df169b4965b6846da
Ali Kılıçoğlu <73752182+AliKilicoglu@users.noreply.github.com>
2020-11-11 22:16:35 +0300

    1.8
    Tablar eklendi artık aynı anda istediğiniz kadar dosya açabilirsiniz

--- Open_Editor.py
+++ Open_Editor.py
@@ -1,15 +1,17 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from dosyasistemi import DosyaSistemi
 from versiyonkontrol  import Versiyon_Kontrol
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QMessageBox,QTabWidget
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from ayarlar import Ui_MainWindow
 import syntax
+import os
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(598, 532)
+
 
         Form.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
         self.pushButton = QtWidgets.QPushButton(Form)
@@ -28,13 +30,26 @@ class Ui_Form(object):
         self.pushButton_4.setGeometry(QtCore.QRect(240, 0, 81, 41))
         self.pushButton_4.setObjectName("pushButton_4")
         self.pushButton_4.clicked.connect(self.ayarlar)
-        self.plainTextEdit = QtWidgets.QPlainTextEdit(Form)
-        self.plainTextEdit.setGeometry(QtCore.QRect(0, 40, 601, 491))
-        self.plainTextEdit.viewport().setProperty("cursor", QtGui.QCursor(QtCore.Qt.IBeamCursor))
-        self.plainTextEdit.setObjectName("plainTextEdit")
-        self.highlighter = syntax.PythonHighlighter(self.plainTextEdit.document())
+
+
         self.pushButton.clicked.connect(self.yaz)
         self.pushButton_2.clicked.connect(self.kaydet)
+        self.tabWidget = QtWidgets.QTabWidget(Form)
+        self.tabWidget.setGeometry(QtCore.QRect(0, 60, 601, 481))
+        self.tabWidget.setObjectName("tabWidget")
+        #self.tab = QtWidgets.QWidget()
+        #self.tab.setObjectName("tab")
+
+        #self.tabWidget.addTab(self.tab, "")
+        #self.tab_2 = QtWidgets.QWidget()
+        #self.tab_2.setObjectName("tab_2")
+        #self.tabWidget.addTab(self.tab_2, "")
+
+
+
+
+
+
 
 
 
@@ -50,20 +65,21 @@ class Ui_Form(object):
         self.pushButton_4.setText(_translate("Form", "Ayarlar"))
 
 
+
     def yaz(self):
-        self.plainTextEdit.clear()
+
         dosya=open("ayarlar.txt","r")
         font_ad=dosya.readline().replace("FONT_ADI=","")
         font_boy=dosya.readline().replace("FONT_BOYU=","")
-        self.plainTextEdit.setFont(QFont(font_ad, int(font_boy)))
+
         update=Versiyon_Kontrol.kontrol()
         if update =="1":
 
             msg = QMessageBox()
 
-            msg.setWindowTitle("Günceleme bulundu!")
+            msg.setWindowTitle("Güncelleme")
             #msg.setText("Yeni günceleme bulundu.\na href <'https://tinyurl.com/y2ruqeh4'>aa</a> güncel sürümü burdan indirebilirsiniz")
-            msg.setText("Yeni güncelleme bulundu!.Güncelemeyi <a href='https://tinyurl.com/y2ruqeh4'Subject=My%20Subject>burdan</a> indirebilirsiniz")
+            msg.setText("Yeni günceleme bulundu!.Güncelemeyi<a href='https://tinyurl.com/y2ruqeh4'Subject=My%20Subject>burdan</a>indirebilirsiniz ")
             msg.setIcon(QMessageBox.Information)
 
 
@@ -76,16 +92,31 @@ class Ui_Form(object):
 
         try:
             dosya=DosyaSistemi.yaz(self)
+
+            self.tab = QtWidgets.QWidget()
+            self.tab.setObjectName("tab")
+
+            self.plainTextEdit = QtWidgets.QPlainTextEdit(self.tab)
+
+            self.tabWidget.addTab(self.tab, str(DosyaSistemi.dosya_adi(self)))
+            self.plainTextEdit.setGeometry(QtCore.QRect(0, 10, 600, 490))
+            self.plainTextEdit.viewport().setProperty("cursor", QtGui.QCursor(QtCore.Qt.IBeamCursor))
+            self.plainTextEdit.setObjectName("plainTextEdit")
+            self.highlighter = syntax.PythonHighlighter(self.plainTextEdit.document())
+
+            self.plainTextEdit.clear()
+            self.plainTextEdit.setFont(QFont(font_ad, int(font_boy)))
             self.plainTextEdit.insertPlainText(dosya.read())
         except:
             pass
-        
+
     def kaydet(self):
         dosya=DosyaSistemi.kaydet(self)
         dosya.write(self.plainTextEdit.toPlainText())
 
     def calistir(self):
         DosyaSistemi.calistir(self)
+
     def ayarlar(self):
         self.pencere=QtWidgets.QMainWindow()
         self.ui=Ui_MainWindow()
@@ -111,20 +142,12 @@ class Ui_Form(object):
 
 
 
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
     ui = Ui_Form()
+
     ui.setupUi(Form)
 
     Form.show()
