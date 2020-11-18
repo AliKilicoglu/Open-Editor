7c18b146e31b87675a552941940a8dc446e5ac99
Ali Kılıçoğlu <73752182+AliKilicoglu@users.noreply.github.com>
2020-11-18 15:03:10 +0300

    1.8.7

--- Open_Editor.py
+++ Open_Editor.py
@@ -14,20 +14,24 @@ class Ui_Form(object):
 
 
         Form.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        self.pushButton_5 = QtWidgets.QPushButton(Form)
+        self.pushButton_5.setGeometry(QtCore.QRect(0, 0, 81, 41))
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_5.clicked.connect(self.yeni)
         self.pushButton = QtWidgets.QPushButton(Form)
-        self.pushButton.setGeometry(QtCore.QRect(0, 0, 81, 41))
+        self.pushButton.setGeometry(QtCore.QRect(80, 0, 81, 41))
         self.pushButton.setObjectName("pushButton")
 
         self.pushButton_2 = QtWidgets.QPushButton(Form)
-        self.pushButton_2.setGeometry(QtCore.QRect(80, 0, 81, 41))
+        self.pushButton_2.setGeometry(QtCore.QRect(160, 0, 81, 41))
         self.pushButton_2.setObjectName("pushButton_2")
 
         self.pushButton_3 = QtWidgets.QPushButton(Form)
-        self.pushButton_3.setGeometry(QtCore.QRect(160, 0, 81, 41))
+        self.pushButton_3.setGeometry(QtCore.QRect(240, 0, 81, 41))
         self.pushButton_3.setObjectName("pushButton_3")
         self.pushButton_3.clicked.connect(self.calistir)
         self.pushButton_4 = QtWidgets.QPushButton(Form)
-        self.pushButton_4.setGeometry(QtCore.QRect(240, 0, 81, 41))
+        self.pushButton_4.setGeometry(QtCore.QRect(320, 0, 81, 41))
         self.pushButton_4.setObjectName("pushButton_4")
         self.pushButton_4.clicked.connect(self.ayarlar)
 
@@ -65,6 +69,7 @@ class Ui_Form(object):
         self.pushButton.setText(_translate("Form", "Aç"))
         self.pushButton_3.setText(_translate("Form", "Çalıştır"))
         self.pushButton_4.setText(_translate("Form", "Ayarlar"))
+        self.pushButton_5.setText(_translate("Form", "Yeni"))
 
 
 
@@ -132,8 +137,29 @@ class Ui_Form(object):
         text=self.plainTextEdit.toPlainText()
         self.plainTextEdit.clear()
         self.plainTextEdit.insertPlainText(text)
+        pass
+    def yeni(self):
+        dosya=open("ayarlar.txt","r")
+        font_ad=dosya.readline().replace("FONT_ADI=","")
+        font_boy=dosya.readline().replace("FONT_BOYU=","")
+        dosya.close()
+        self.dosya_konumu = DosyaSistemi.yeni(self)
+
+        self.tab = QtWidgets.QWidget()
+        self.tab.setObjectName("tab")
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.tab)
 
 
+        self.tabWidget.addTab(self.tab, "untitled.py")
+        self.plainTextEdit.setGeometry(QtCore.QRect(0, 10, 600, 490))
+        self.plainTextEdit.viewport().setProperty("cursor", QtGui.QCursor(QtCore.Qt.IBeamCursor))
+        self.plainTextEdit.setObjectName("plainTextEdit")
+        self.highlighter = syntax.PythonHighlighter(self.plainTextEdit.document())
+
+        self.plainTextEdit.clear()
+        self.plainTextEdit.setFont(QFont(font_ad, int(font_boy)))
+        self.plainTextEdit.insertPlainText(self.dosya_konumu.read())
+        self.dosya_konumu=str(self.dosya_konumu)
 
 
 
@@ -154,3 +180,4 @@ if __name__ == "__main__":
 
     Form.show()
     sys.exit(app.exec_())
+
